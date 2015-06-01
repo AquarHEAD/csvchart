d07f056be9f4322a83af849e911de72837256325
AquarHEAD Lou <aquarhead@gmail.com>
2015-06-01 16:07:28 +0800

    [On #2] Show events in 2 lists.

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,4 +1,5 @@
-function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings) {
+function app(frame_data, gt_data, rt_data, chart_id, overview_id, leftevents_id, rightevents_id, event_markings) {
+  // Trick? to allow empty event_markings
   var ems = [];
   if (typeof event_markings !== 'undefined') {
     ems = event_markings.map( function (x) { return eval(x) })
@@ -18,9 +19,6 @@ function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings
       backgroundColor: "#EAE8FF"
     },
     colors: ["#FE7F2D", "#51A8DD", "#439775"],
-    xaxis: {
-      show: false
-    },
     yaxis: {
       min: 0,
       max: 45,
@@ -33,6 +31,8 @@ function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings
       mode: "x"
     },
     grid: {
+      hoverable: true,
+      autoHighlight: false,
       backgroundColor: "#222",
       aboveData: false,
       markings: [
@@ -81,13 +81,17 @@ function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings
 
   function draw_chart() {
     var data = [];
+
+    // Only add what should be plotted
     $("#choices").find("input:checked").each(function () {
       key = $(this).attr("name");
       if (key && dataset[key]) {
         data.push(dataset[key]);
       }
     });
+
     $(function(){
+      // Plot chart
       var chart = $(chart_id);
       var plot = $.plot(chart_id, data, chart_options);
 
@@ -99,18 +103,11 @@ function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings
         chart.append('<div style="position:absolute;left:' + (pos30.left + 4) + 'px;top:' + (pos30.top-15) + 'px;color:#FF0;font-size:smaller">30 FPS</div>');
       }
 
-      function add_event_labels() {
-        ems.forEach( function(em) {
-          var pos = plot.pointOffset({ x: em.xaxis.from, y: 45});
-          chart.append('<div style="transform: rotate(-45deg);transform-origin: left top 0;position: absolute;left:' + (pos.left + 4) + 'px;top:' + (pos.top - 12) + 'px;color:#000;font-size:smaller">' + em.description + '</div>');
-        });
-      }
-
       add_fps_label();
-      add_event_labels();
 
       var overview = $.plot(overview_id, data, overview_options);
 
+      // Bind chart zooming
       $(chart_id).bind("plotselected", function(event, ranges) {
         plot = $.plot(chart_id, data, $.extend(true, {}, chart_options, {
           xaxis: {
@@ -119,28 +116,61 @@ function app(frame_data, gt_data, rt_data, chart_id, overview_id, event_markings
           }
         }));
         add_fps_label();
-        add_event_labels();
         overview.setSelection(ranges, true);
       });
 
       $(chart_id).bind("plotunselected", function(event, ranges) {
         plot = $.plot(chart_id, data, chart_options);
         add_fps_label();
-        add_event_labels();
         overview.clearSelection(true);
       });
 
-      $(overview_id).bind("plotselected", function(event, ranges)
-      {
+      // Bind overview
+      $(overview_id).bind("plotselected", function(event, ranges) {
           plot.setSelection(ranges);
       });
-      $(overview_id).bind("plotunselected", function(event)
-      {
+
+      $(overview_id).bind("plotunselected", function(event) {
         plot = $.plot(chart_id, data, chart_options);
         add_fps_label();
-        add_event_labels();
         plot.clearSelection(true);
       });
+
+      // Bind events display when hovering
+      var updateEventsTimeout = null;
+      var latestPosition = null;
+
+      function updateEvents() {
+        var pos = latestPosition;
+        var axes = plot.getAxes();
+        if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+          pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
+          return ;
+        }
+
+        // Add events +-6% around mouse position (to different well)
+        $(leftevents_id).empty();
+        ems.filter(function(em) {
+          return (em.xaxis.from <= pos.x) && (em.xaxis.from <= axes.xaxis.max) && ((pos.x - em.xaxis.from) < axes.xaxis.max * 0.06);
+        }).forEach(function(em) {
+          $(leftevents_id).append('<p>' + em.xaxis.from + ': ' + em.description + '</p>');
+        });
+        $(rightevents_id).empty();
+        ems.filter(function(em) {
+          return (em.xaxis.from > pos.x) && (em.xaxis.from <= axes.xaxis.max) && ((em.xaxis.from - pos.x) < axes.xaxis.max * 0.06);
+        }).forEach(function(em) {
+          $(rightevents_id).append('<p>' + em.xaxis.from + ': ' + em.description + '</p>');
+        });
+        updateEventsTimeout = null;
+      }
+
+      $(chart_id).bind("plothover", function(event, pos, item) {
+        latestPosition = pos;
+        if (!updateEventsTimeout) {
+          updateEventsTimeout = setTimeout(updateEvents, 50);
+        }
+      });
+
     });
   }
 
